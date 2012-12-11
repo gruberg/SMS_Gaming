a22664ca7106206554adb014bc4311aeddf76ea3
Lorenzo Randé <lolorenzo19@gmail.com>
2012-12-11 13:48:56 +0100

    Added SMS related permissions + SMSParser.parse(message)

--- src/mobserv/smsgaming/SMSParser.java
+++ src/mobserv/smsgaming/SMSParser.java
@@ -2,15 +2,27 @@ package mobserv.smsgaming;
 
 import java.util.ArrayList;
 
+import android.util.Log;
+
 public class SMSParser {
 
 	ArrayList<Group> groups = new ArrayList<Group>();
 	
 	SMSParser() {
 		groups = null;
+		SMSReceiver.setParser(this);
 	}
 	
 	SMSParser(ArrayList<Group> groups) {
 		this.groups = groups;
+		SMSReceiver.setParser(this);
+	}
+	
+	/**
+	 * This method is called by SMSReceiver when messages are incoming
+	 * @param msg : String containing one or more SMS
+	 */
+	public void parse(String msg){
+		Log.i("SMSParser", msg);
 	}
 }
