4d4143deece26b90d07f129b24a2c6bbee554ed7
gruberg <guillaume.gruber@laposte.net>
2013-02-13 12:31:34 +0100

    fix add groups

--- src/mobserv/smsgaming/JoinGroupActivity.java
+++ src/mobserv/smsgaming/JoinGroupActivity.java
@@ -1,5 +1,9 @@
 package mobserv.smsgaming;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -9,7 +13,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,6 +24,7 @@ import android.widget.Toast;
 
 public class JoinGroupActivity extends Activity {
 
+	
 	EditText mEdit;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -59,12 +66,13 @@ public class JoinGroupActivity extends Activity {
 	}
 
 	
-	public void joinThatGroup1(View view){
+	public void joinThatGroup1(View view){		
 		Context context = getApplicationContext();
 		CharSequence text = "You joined the weekly challenge group";
 		int duration = Toast.LENGTH_SHORT;
 		Toast toast = Toast.makeText(context, text, duration);
 		toast.show();
+		
 
 	}
 
@@ -79,39 +87,226 @@ public class JoinGroupActivity extends Activity {
 	}
 
 	public class NoGroupDialogFragment extends DialogFragment {
-		@Override
+		
+		
 		public Dialog onCreateDialog(Bundle savedInstanceState) {
 			// Use the Builder class for convenient dialog construction
 			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 			builder.setMessage(R.string.noGroup)
 			.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+				ArrayList<Group> groups = new ArrayList<Group>();
+				ArrayList<Player> players = new ArrayList<Player>();
+				ArrayList<Challenge> challenges = new ArrayList<Challenge>();
+				
+				String separator = "_;_";
+
 				public void onClick(DialogInterface dialog, int id) {
-					Group newgroup = new Group();
-					newgroup.name = mEdit.getText().toString();
-					Context context = getApplicationContext();
-					
 					
 					String mEmail;
+					
+					String groupname = mEdit.getText().toString();
 				    SharedPreferences sharedPreferences = getSharedPreferences("login",MODE_PRIVATE);
 					mEmail= sharedPreferences.getString("USER", "");
 					User u = new User();
-					u.setGroup(newgroup.name);
+					u.setGroup(groupname);
 					u.setMail(mEmail);
-					
 					final UserController uc = new UserController();
 					//Log.v("JoinGroupActivity", "UC instanciated");
 					try {
 					    uc.create(u);
 					} catch (Exception e) {
-					    return;
+					    //return;
+						Log.e("JoinGroupActivity", "exception uc.create");
 					}
-					
-					
-					CharSequence text = "Group "+ newgroup.name + " created";
+					this.addGroup(groupname);
+					Context context = getApplicationContext();
+					CharSequence text = "Group "+ groupname + " created";
 					int duration = Toast.LENGTH_SHORT;
 					Toast toast = Toast.makeText(context, text, duration);
 					toast.show();
 				}
+				public void readData() {
+					int i,j;
+
+					challenges.clear();
+					players.clear();
+					groups.clear();
+
+					SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+					Set<String> voidset = new HashSet<String>();
+					voidset.add("null");
+					// gets all groups
+					Set<String> set_groups_prov = preferences.getStringSet("groups", voidset);
+					// if not void, then loop on the groups to get their data
+					if (set_groups_prov!=voidset){
+						for (i=0;i<set_groups_prov.size();i++) {
+							String g_name_prov = set_groups_prov.toArray()[i].toString();
+							if (getGroup(g_name_prov)==null){
+								Group group_prov = new Group(g_name_prov);
+								//System.out.println("new group :"+g_name_prov);	
+
+
+								Set<String> set_p_prov = preferences.getStringSet(g_name_prov+separator+"P", voidset);
+								Set<String> set_c_prov = preferences.getStringSet(g_name_prov+separator+"C", voidset);
+								String bet_prov = preferences.getString(g_name_prov+separator+"B","");
+								//System.out.println(set_p_prov);
+								//System.out.println(set_c_prov);
+								//System.out.println(bet_prov);
+								if (set_p_prov!=voidset) {
+									for (j=0;j<set_p_prov.size();j++) {
+
+										String[] p_prov = set_p_prov.toArray()[j].toString().split(separator);
+										String p_name_prov = p_prov[0];
+										String p_number_prov = p_prov[1];
+										boolean p_isuser_prov = Boolean.parseBoolean(p_prov[2]);
+										int p_score_prov = Integer.parseInt(p_prov[3]);
+
+										//System.out.println("new player :"+p_name_prov+" with n*:"+p_number_prov);
+
+										if (getPlayer(p_name_prov,p_number_prov)==null) {
+											Player player_prov = new Player(getActivity(),p_name_prov,p_number_prov,p_isuser_prov);
+											player_prov.setScore(g_name_prov, p_score_prov);
+											this.players.add(player_prov);
+											group_prov.addPlayer(player_prov);
+										}
+										else {
+											Player player_prov;
+											player_prov = getPlayer(p_name_prov,p_number_prov);
+											player_prov.setScore(g_name_prov, p_score_prov);
+											group_prov.addPlayer(player_prov);
+										}
+									}
+								}
+
+								if (set_c_prov!=voidset) {
+									for (j=0;j<set_c_prov.size();j++) {
+										String c_name_prov = set_c_prov.toArray()[j].toString().split(separator)[0];
+										String c_points_prov = set_c_prov.toArray()[j].toString().split(separator)[1];
+										boolean c_completed_prov = Boolean.parseBoolean(set_c_prov.toArray()[j].toString().split(separator)[2]);
+										if (getChallenge(c_name_prov,g_name_prov)==null) {
+											//System.out.println("new c:"+c_name_prov+" value:"+c_points_prov+" done?:"+c_completed_prov);
+											Challenge c_prov = new Challenge(getActivity(), c_name_prov,Integer.parseInt(c_points_prov), g_name_prov,c_completed_prov);
+											this.challenges.add(c_prov);
+											group_prov.addChallenge(c_prov);
+										}
+									}
+								}	
+								
+								if (!bet_prov.equals("")) {
+									group_prov.setBet(bet_prov);
+								}
+								groups.add(group_prov);
+							}
+						}
+					}
+				}
+
+
+				public void addGroup(String groupname){
+					
+					//System.out.println("in addgroup");
+
+					this.readData();
+					//this.printData();
+
+					Set<String> voidset = new HashSet<String>();
+					voidset.add("null");
+					SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+					SharedPreferences.Editor editor = preferences.edit();
+
+					Set<String> set_groups_prov = preferences.getStringSet("groups", voidset);
+					set_groups_prov.add(groupname);
+					editor.putStringSet("groups", set_groups_prov);
+					
+					Player user = getUser();
+					Set<String> groupP = new HashSet<String>();
+					groupP.add(user.getName()+separator+user.getPhone_number()+separator+"true"+separator+"0");
+					editor.putStringSet(groupname+separator+"P", groupP);
+
+					Set<String> groupC = new HashSet<String>();
+					groupC.add("SpongeBob"+separator+"30"+separator+false);
+					groupC.add("Eurecom"+separator+"20"+separator+false);
+					editor.putStringSet(groupname+separator+"C", groupC);
+
+					editor.putString(groupname+separator+"B", "Winner gets free cinema ticket");
+					
+					editor.commit();
+
+					//this.readData();	
+					this.printData();
+
+
+
+				}
+				
+				public Player getPlayer(String name, String number) {
+					Player ret = null;
+					for (Player player : this.players) {
+						if ((player.getName().equals(name))&&(player.getPhone_number().equals(number))) {
+							ret = player;
+						}
+					}
+					return ret;
+				}
+
+				public Group getGroup(String groupname) {
+					Group ret = null;
+					for (Group group : this.groups) {
+						if (group.getName().equals(groupname)) {
+							ret = group;
+						}
+					}
+					return ret;
+				}
+
+				public Challenge getChallenge(String objective,String groupname){
+					Challenge ret = null;
+					for (Challenge challenge: this.challenges) {
+						if ((challenge.getObjective().equals(objective))&&(challenge.getGroupname().equals(groupname))) {
+							ret = challenge;
+						}
+					}
+					return ret;
+				}
+
+				public ArrayList<Group> getGroups() {
+					return groups;
+				}
+
+				public ArrayList<Player> getPlayers() {
+					return players;
+				}
+
+				public ArrayList<Challenge> getChallenges() {
+					return challenges;
+				}
+
+				public Player getUser(){
+					Player ret = null;
+					for (Player player : players) {
+						if (player.isUser()) ret = player;
+					}
+					return ret;
+				}
+				
+				public void printData() {
+					System.out.println("nb groups:"+groups.size());
+					for(Group group : groups) {
+						Log.d("printData",group.toString());
+					}
+					System.out.println("nb players:"+players.size());
+					for (Player player : players) {
+						Log.d("printData",player.toString());
+					}
+					System.out.println("nb challenges:"+challenges.size());
+					for (Challenge challenge: challenges) {
+						Log.d("printData",challenge.toString());
+					}
+				}
+				
+				
+				
 			})
 			.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int id) {
@@ -121,12 +316,15 @@ public class JoinGroupActivity extends Activity {
 			// Create the AlertDialog object and return it
 			return builder.create();
 		}
+		
+		
 	}
 
 	public void confirmGroupCreation() {
 		DialogFragment newFragment = new NoGroupDialogFragment();
 		newFragment.show(getFragmentManager(), "nogroup");
 	}
+	
+	
 
 }
-
--- src/mobserv/smsgaming/MainActivity.java
+++ src/mobserv/smsgaming/MainActivity.java
@@ -10,8 +10,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -27,6 +29,7 @@ public class MainActivity extends Activity {
 	ArrayList<Group> groups = new ArrayList<Group>();
 	ArrayList<Player> players = new ArrayList<Player>();
 	ArrayList<Challenge> challenges = new ArrayList<Challenge>();
+	Boolean force_clean = false;
 	
 	String separator = "_;_";
 	@Override
@@ -35,64 +38,88 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		System.out.println("in oncreate");
+
+
 		lvListe = (ListView)findViewById(R.id.listView1);
 		
 
-		//tests de c�sar
+		//tests de cesar
 		Set<String> voidset = new HashSet<String>();
 		voidset.add("null");
 
 		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
 		SharedPreferences.Editor editor = preferences.edit();
-		editor.clear();
 		
-		Date date = new Date();
-		editor.putString("date", date.toString());
+
+		Set<String> set_groups_prov = preferences.getStringSet("groups", voidset);
+		if (set_groups_prov == voidset || force_clean){
+			editor.clear();
+			
+			Date date = new Date();
+			editor.putString("date", date.toString());
+			
+			Set<String> test = new HashSet<String>();
+			test.add("group1");
+			test.add("group2");
+			test.add("group3");
+			editor.putStringSet("groups", test);
+
+			Set<String> group1P = new HashSet<String>();
+			group1P.add("John"+separator+"0672838272"+separator+"true"+separator+"3");
+			group1P.add("Bob"+separator+"0630000000"+separator+"false"+separator+"4");
+			group1P.add("Frank"+separator+"0649857984"+separator+"false"+separator+"56");
+
+			editor.putStringSet("group1"+separator+"P", group1P);
+
+			
+			Set<String> group1C = new HashSet<String>();
+			group1C.add("peperroni"+separator+"30"+separator+false);
+			group1C.add("pizza"+separator+"50"+separator+false);
+			editor.putStringSet("group1"+separator+"C", group1C);
+
+			
+			Set<String> group2P = new HashSet<String>();
+			group2P.add("John"+separator+"0672838272"+separator+"true"+separator+"12");
+			group2P.add("Alice"+separator+"0632340000"+separator+"false"+separator+"9");
+			editor.putStringSet("group2"+separator+"P", group2P);
+
+			Set<String> group2C = new HashSet<String>();
+			group2C.add("Madonna"+separator+"40"+separator+false);
+			group2C.add("OMG"+separator+"10"+separator+false);
+			editor.putStringSet("group2"+separator+"C", group2C);
+			
+			Set<String> group3P = new HashSet<String>();
+			group3P.add("John"+separator+"0672838272"+separator+"true"+separator+"12");
+			group3P.add("Alice"+separator+"0632340000"+separator+"false"+separator+"9");
+			editor.putStringSet("group3"+separator+"P", group3P);
+
+			Set<String> group3C = new HashSet<String>();
+			group3C.add("Banana"+separator+"40"+separator+false);
+			group3C.add("Tomatoe"+separator+"10"+separator+false);
+			editor.putStringSet("group3"+separator+"C", group3C);
+
+			editor.putString("group3"+separator+"B", "Winner wins !");
+
+			editor.putString("group1"+separator+"B", "Last one pays pizza for everyone");
+			editor.putString("group2"+separator+"B", "Winner gets ticket to concert");
+
+			editor.commit();
+		}
+
 		
-		Set<String> test = new HashSet<String>();
-		test.add("group1");
-		test.add("group2");
-		editor.putStringSet("groups", test);
-
-		Set<String> group1P = new HashSet<String>();
-		group1P.add("player1"+separator+"0672838272"+separator+"true"+separator+"3");
-		group1P.add("player2"+separator+"0630000000"+separator+"false"+separator+"4");
-		group1P.add("player3"+separator+"0649857984"+separator+"false"+separator+"56");
-
-		editor.putStringSet("group1"+separator+"P", group1P);
-
-		Set<String> group1C = new HashSet<String>();
-		group1C.add("dothis1"+separator+"30"+separator+false);
-		group1C.add("dothis2"+separator+"50"+separator+false);
-		editor.putStringSet("group1"+separator+"C", group1C);
-
-		Set<String> group2P = new HashSet<String>();
-		group2P.add("player1"+separator+"0672838272"+separator+"true"+separator+"12");
-		group2P.add("player4"+separator+"0632340000"+separator+"false"+separator+"9");
-		editor.putStringSet("group2"+separator+"P", group2P);
-
-		Set<String> group2C = new HashSet<String>();
-		group2C.add("dothis1"+separator+"40"+separator+false);
-		group2C.add("dothis3"+separator+"10"+separator+false);
-		editor.putStringSet("group2"+separator+"C", group2C);
-
-		editor.putString("group1"+separator+"B", "Last one pays pizza for everyone");
-		editor.putString("group2"+separator+"B", "Winner gets ticket to concert");
-
-		editor.commit();
 
 		this.readData();	
 		this.printData();
 		//getUser().challengeCompleted(getGroup("group1"),challenges.get(0));
 		
-		//fin tests c�sar
+		//fin tests c���sar
 
 		SMSParser parser = new SMSParser(getUser(), groups);
 		SMSReceiver.setParserChall(parser, challenges, this);
 		for (Challenge chall : challenges){//Look if any challenge was completed
 		    parser.searchSMS(chall, this);
 		}
-
 		
 		GroupAdapter adapter = new GroupAdapter(this, groups);
 
@@ -121,6 +148,56 @@ public class MainActivity extends Activity {
 			}
 		});
 
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	protected void onResume() {
+		super.onResume();
+		System.out.println("in onResume");
+		this.readData();	
+		this.printData();
+		
+		GroupAdapter adapter = new GroupAdapter(this, groups);
+
+		lvListe.setAdapter(adapter);
+
+		lvListe.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				// selected item
+				TextView group_view = (TextView) ((LinearLayout) view).getChildAt(0);
+				// Launching new Activity on selecting single List Item
+				Intent i = new Intent(getApplicationContext(), GroupItem.class);
+				// sending data to new activity
+				i.putExtra("name",group_view.getText().toString());
+
+				/*
+	              ArrayList<String> playersinfos = new ArrayList<String>();
+	              playersinfos.add("Bob"+separator+"14");
+	              playersinfos.add("Jenny"+separator+"63");
+
+
+	              i.putExtra("players", playersinfos);
+				 */
+				startActivity(i);
+			}
+		});
 	}
 
 	@Override
@@ -158,12 +235,15 @@ public class MainActivity extends Activity {
 				String g_name_prov = set_groups_prov.toArray()[i].toString();
 				if (getGroup(g_name_prov)==null){
 					Group group_prov = new Group(g_name_prov);
-					//Toast.makeText(getBaseContext(),"new group :"+g_name_prov,Toast.LENGTH_SHORT).show();	
+					//System.out.println("new group :"+g_name_prov);	
 
 
 					Set<String> set_p_prov = preferences.getStringSet(g_name_prov+separator+"P", voidset);
 					Set<String> set_c_prov = preferences.getStringSet(g_name_prov+separator+"C", voidset);
 					String bet_prov = preferences.getString(g_name_prov+separator+"B","");
+					//System.out.println(set_p_prov);
+					//System.out.println(set_c_prov);
+					//System.out.println(bet_prov);
 					if (set_p_prov!=voidset) {
 						for (j=0;j<set_p_prov.size();j++) {
 
@@ -173,7 +253,7 @@ public class MainActivity extends Activity {
 							boolean p_isuser_prov = Boolean.parseBoolean(p_prov[2]);
 							int p_score_prov = Integer.parseInt(p_prov[3]);
 
-							//Toast.makeText(getBaseContext(),"new player :"+p_name_prov+" with n�:"+p_number_prov,Toast.LENGTH_SHORT).show();
+							//System.out.println("new player :"+p_name_prov+" with n*:"+p_number_prov);
 
 							if (getPlayer(p_name_prov,p_number_prov)==null) {
 								Player player_prov = new Player(this,p_name_prov,p_number_prov,p_isuser_prov);
@@ -196,7 +276,7 @@ public class MainActivity extends Activity {
 							String c_points_prov = set_c_prov.toArray()[j].toString().split(separator)[1];
 							boolean c_completed_prov = Boolean.parseBoolean(set_c_prov.toArray()[j].toString().split(separator)[2]);
 							if (getChallenge(c_name_prov,g_name_prov)==null) {
-								//Toast.makeText(getBaseContext(),"new c:"+c_name_prov+" value:"+c_points_prov+" done?:"+c_completed_prov,Toast.LENGTH_SHORT).show();
+								//System.out.println("new c:"+c_name_prov+" value:"+c_points_prov+" done?:"+c_completed_prov);
 								Challenge c_prov = new Challenge(this, c_name_prov,Integer.parseInt(c_points_prov), g_name_prov,c_completed_prov);
 								this.challenges.add(c_prov);
 								group_prov.addChallenge(c_prov);
@@ -214,6 +294,35 @@ public class MainActivity extends Activity {
 	}
 
 
+	public void AddGroup(String groupname){
+		Set<String> voidset = new HashSet<String>();
+		voidset.add("null");
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		SharedPreferences.Editor editor = preferences.edit();
+		editor.clear();
+		Set<String> set_groups_prov = preferences.getStringSet("groups", voidset);
+		set_groups_prov.add(groupname);
+		editor.putStringSet("groups", set_groups_prov);
+		
+		Player user = getUser();
+		Set<String> groupP = new HashSet<String>();
+		groupP.add(user.getName()+separator+user.getPhone_number()+separator+"true"+separator+"0");
+		editor.putStringSet(groupname+separator+"P", groupP);
+
+		Set<String> groupC = new HashSet<String>();
+		groupC.add("Banana"+separator+"30"+separator+false);
+		groupC.add("Tomatoe"+separator+"20"+separator+false);
+		editor.putStringSet(groupname+separator+"C", groupC);
+
+		editor.putString(groupname+separator+"B", "Winner gets free cinema ticket");
+		
+		editor.commit();
+
+		this.readData();	
+
+
+
+	}
 	public Player getPlayer(String name, String number) {
 		Player ret = null;
 		for (Player player : this.players) {
